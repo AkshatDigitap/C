29e336f5879836a4f49d1d5374e373e19d8a3a87
Akshat Jaiswal <akshat.jaiswal@digitap.ai>
2023-11-23 20:17:26 +0530

    Initial Commit

---
+++ printMyName.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+    char name2[] = "akshat";
+    char name = 'A';
+    printf("Hello there, My name is Akshat Jaiswal");
+    printf("\n and first letter of my name is %c \n", name);
+    printf("%s", name2);
+}
